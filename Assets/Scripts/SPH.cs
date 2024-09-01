0b1df4c2e37c3b2deabfe9a5265b88cff09680bf
Mi <miro.brodlova@gmail.com>
2024-09-01 17:09:14 +0200

    Add forces and pressure simulation

--- Assets/Scripts/SPH.cs
+++ Assets/Scripts/SPH.cs
@@ -30,6 +30,7 @@ public class SPH : MonoBehaviour
     public Vector3 boxSize = new Vector3(4,10,3);
     public Vector3 spawnCenter;
     public float particleRadius = 0.1f;
+    public float spawnJitter = 0.2f;
 
     [Header("Particle Rendering")]
     public Mesh particleMesh;
@@ -40,9 +41,21 @@ public class SPH : MonoBehaviour
     public ComputeShader shader;
     public Particle[] particles;
 
+    [Header("Fluid Constants")]
+    public float boundDamping = -0.3f;
+    public float viscosity = -0.003f;
+    public float particleMass = 1f;
+    public float gasConstant = 2f;
+    public float restingDensity = 1f;
+    public float timestep = 0.007f;
+
     //Private variables
     private ComputeBuffer _argsBuffer;
     private ComputeBuffer _particlesBuffer;
+    private int integralKernel;
+    private int computeForceKernel;
+    private int densityPressureKernel;
+
 
     private static readonly int SizeProperty = Shader.PropertyToID("_size");
     private static readonly int ParticlesBuggerPropery = Shader.PropertyToID("_particlesBuffer");
@@ -67,6 +80,16 @@ public class SPH : MonoBehaviour
 
     }
 
+    private void FixedUpdate()
+    {
+        shader.SetVector("boxSize", boxSize);
+        shader.SetFloat("timestep", timestep);
+
+        shader.Dispatch(densityPressureKernel, totalParticles / 100, 1, 1);
+        shader.Dispatch(computeForceKernel, totalParticles / 100, 1, 1);
+        shader.Dispatch(integralKernel, totalParticles / 100, 1, 1);
+    }
+
     private void Awake()
     {
         SpawnParticlesInBox();
@@ -87,8 +110,36 @@ public class SPH : MonoBehaviour
         // Setup Particles
         _particlesBuffer = new ComputeBuffer(totalParticles, 44);
         _particlesBuffer.SetData(particles);
+
+        SetupComputeBiffers();
     }
 
+    public void SetupComputeBiffers()
+    {
+        integralKernel = shader.FindKernel("Integrate");
+        computeForceKernel = shader.FindKernel("ComputeForces");
+        densityPressureKernel = shader.FindKernel("ComputeDensityPressure");
+
+
+        shader.SetInt("particleLength", totalParticles);
+        shader.SetFloat("particleMass", particleMass);
+        shader.SetFloat("viscosity",viscosity);
+        shader.SetFloat("gasConstant",gasConstant);
+        shader.SetFloat("restDensity",restingDensity);
+        shader.SetFloat("boundDamping", boundDamping);
+        shader.SetFloat("pi",Mathf.PI);
+        shader.SetVector("boxSize", boxSize);
+        shader.SetFloat("radius", particleRadius);
+        shader.SetFloat("radius2", particleRadius * particleRadius);
+        shader.SetFloat("radius3", particleRadius * particleRadius * particleRadius);
+        shader.SetFloat("radius4", particleRadius * particleRadius * particleRadius * particleRadius);
+        shader.SetFloat("radius5", particleRadius * particleRadius * particleRadius * particleRadius * particleRadius);
+
+        shader.SetBuffer( integralKernel, "_particles", _particlesBuffer);
+        shader.SetBuffer(computeForceKernel, "_particles", _particlesBuffer);
+        shader.SetBuffer( densityPressureKernel, "_particles", _particlesBuffer);
+
+    } 
 
     private void SpawnParticlesInBox()
     {
@@ -102,6 +153,7 @@ public class SPH : MonoBehaviour
                 for (int z = 0; z < numToSpawn.z; z++)
                 {
                     Vector3 spawnPos = spawnPoint + new Vector3(x * particleRadius * 2, y * particleRadius * 2, z * particleRadius * 2);
+                    spawnPos += Random.onUnitSphere * particleRadius * spawnJitter;
                     Particle p = new Particle()
                     {
                         position = spawnPos,
